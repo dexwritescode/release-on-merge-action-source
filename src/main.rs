79880d17f3c6e696cc1e1a59e7fef8b892705cd9
Dex <dexwritescode@gmail.com>
2023-09-03 22:21:42 -0400

    Print env vars

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{env, process::exit};
+
+use octorust::{auth::Credentials, Client};
+
 fn main() {
-    println!("Hello, world!");
+    let github_token = env::var("GITHUB_TOKEN").ok();
+    println!("GITHUB_TOKEN: {:?}", &github_token);
+    let github_api_url = env::var("GITHUB_API_URL").ok();    
+    println!("GITHUB_API_URL: {:?}", github_api_url);
+
+    let github_token = match github_token {
+        Some(value) => value,
+        None => {
+          println!("GITHUB_API_URL is empty. Exiting.");
+          exit(1);
+        },
+    };
+
+
+    let _github = Client::new(
+        String::from("user-agent-name"),
+        Credentials::Token(
+          github_token
+        ),
+      );
+      
+      env::set_var("GITHUB_OUTPUT", "The version we just incremented");
 }
