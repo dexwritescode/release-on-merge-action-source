84962736e1d36ab8004fda5341ebb239169ef12e
Dex <dexwritescode@proton.me>
2023-10-01 10:04:11 -0400

    Check if args has the input

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
-use std::{env, process::exit};
 use std::fs::write;
+use std::{env, process::exit};
 
 fn main() {
-    let github_output_file = env::var("GITHUB_OUTPUT").unwrap();
-    eprintln!("GITHUB_OUTPUT: {:?}", &github_output_file);
-    let github_token = env::var("GITHUB_TOKEN").ok();
-    eprintln!("GITHUB_TOKEN: {:?}", &github_token);
-    
-    let github_api_url = env::var("GITHUB_API_URL").ok();    
-    println!("GITHUB_API_URL: {:?}", github_api_url);
+    let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
+    eprintln!("GITHUB_OUTPUT: {:?}", &github_output_path);
 
-    let version_increment_strategy = env::var("INPUT_VERSION-INCREMENT-STRATEGY").ok();    
-    println!("INPUT_VERSION-INCREMENT-STRATEGY: {:?}", version_increment_strategy);
-
-    let _github_token = match github_token {
-      Some(value) => value,
-      None => {
-        eprintln!("GITHUB_TOKEN is empty. Exiting.");
-        exit(1);
-      },
-    };
-      
     let version = "0.1.0";
     let output_text = format!("semver={version}");
     eprintln!("Writing: {}", output_text);
-    write(github_output_file, output_text).unwrap();
+    write(&github_output_path, output_text).unwrap();
+
+    let args: Vec<String> = env::args().collect();
+    let version_increment_strategy = &args[1];
+
+    if !version_increment_strategy.is_empty() {
+        eprintln!("Version Increment Strategy: {version_increment_strategy}");
+        write(
+            github_output_path,
+            format!("version_increment_strategy={version_increment_strategy}"),
+        )
+        .unwrap();
+        exit(1);
+    }    
 }
