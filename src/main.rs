9604126e09c07e7eeb28b6238050c5866434fc09
Dex <dexwritescode@proton.me>
2023-10-01 10:32:46 -0400

    Print INPUT_VERSION-INCREMENT-STRATEGY env var

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ fn main() {
     let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
     eprintln!("GITHUB_OUTPUT: {:?}", &github_output_path);
 
+    let vis_env = env::var("INPUT_VERSION-INCREMENT-STRATEGY").ok();
+    if vis_env.is_some() {
+        eprintln!("INPUT_VERSION-INCREMENT-STRATEGY: {:?}", vis_env);
+    }
+
     let version = "0.1.0";
     let output_text = format!("semver={version}");
     eprintln!("Writing: {}", output_text);
@@ -21,5 +26,5 @@ fn main() {
         )
         .unwrap();
         exit(1);
-    }    
+    }
 }
