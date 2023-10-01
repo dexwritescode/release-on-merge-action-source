b21994f066c70808e90ab53c470b362c5a8fee9b
Dex <dexwritescode@proton.me>
2023-10-01 19:20:42 -0400

    Check GITHUB_TOKEN env var

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-use std::env;
 use std::fs::write;
+use std::{env, process::exit};
 
 fn main() {
     let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
     eprintln!("GITHUB_OUTPUT: {:?}", &github_output_path);
 
-    let vis_env = env::var("INPUT_VERSION-INCREMENT-STRATEGY").ok();
-    if vis_env.is_some() {
-        eprintln!("INPUT_VERSION-INCREMENT-STRATEGY: {:?}", vis_env);
-    }
+    let version_increment_strategy = match env::var("INPUT_VERSION-INCREMENT-STRATEGY") {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("INPUT_VERSION-INCREMENT-STRATEGY should be set");
+            exit(1);
+        }
+    };
+    eprintln!(
+        "INPUT_VERSION-INCREMENT-STRATEGY: {:?}",
+        version_increment_strategy
+    );
+
+    let _github_token = match env::var("GITHUB_TOKEN") {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("GITHUB_TOKEN is empty!");
+            exit(1);
+        }
+    };
+    eprintln!("GITHUB_TOKEN is set");
 
     let version = "0.1.0";
     let output_text = format!("semver={version}");
