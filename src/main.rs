45a1cc47153867199ee616ed899c3e7e0198b945
Dex <dexwritescode@proton.me>
2023-09-30 21:11:02 -0400

    Print lines in actions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::{env, process::exit};
 use std::fs::write;
-use octorust::{auth::Credentials, Client};
 
 fn main() {
     let github_output_file = env::var("GITHUB_OUTPUT").unwrap();
     let github_token = env::var("GITHUB_TOKEN").ok();
-    println!("GITHUB_TOKEN: {:?}", &github_token);
+    eprintln!("GITHUB_TOKEN: {:?}", &github_token);
     
     let github_api_url = env::var("GITHUB_API_URL").ok();    
     println!("GITHUB_API_URL: {:?}", github_api_url);
@@ -13,21 +12,16 @@ fn main() {
     let version_increment_strategy = env::var("INPUT_VERSION-INCREMENT-STRATEGY").ok();    
     println!("INPUT_VERSION-INCREMENT-STRATEGY: {:?}", version_increment_strategy);
 
-    let github_token = match github_token {
+    let _github_token = match github_token {
       Some(value) => value,
       None => {
-        println!("GITHUB_TOKEN is empty. Exiting.");
+        eprintln!("GITHUB_TOKEN is empty. Exiting.");
         exit(1);
       },
     };
-
-    let _github = Client::new(
-        String::from("user-agent-name"),
-        Credentials::Token(
-          github_token
-        ),
-      );
       
-      let version = "0.1.0";
-      write(github_output_file, format!("semver={version}")).unwrap();
+    let version = "0.1.0";
+    let output_text = format!("semver={version}");
+    eprintln!("Writing: {}", output_text);
+    write(github_output_file, output_text).unwrap();
 }
