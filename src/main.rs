3fded82ec1e87dec1a9b76742e28ee4c8bc35e67
Dex <dexwritescode@proton.me>
2023-10-08 16:46:18 -0400

    Get release (#9)
    * Get release
    
    * If there is no release, default to 0.1.0
    
    * Get default release version
    
    * Test the default version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use octocrab::{Error, Octocrab};
 use std::{env, process::exit};
 
 pub mod writer;
@@ -5,7 +6,8 @@ pub mod writer;
 const _GITHUB_API_VERSION_HEADER: &str = "X-GitHub-Api-Version";
 const _GITHUB_API_VERSION_VALUE: &str = "2022-11-28";
 
-fn main() {
+#[tokio::main]
+async fn main() -> octocrab::Result<()> {
     let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
     eprintln!("GITHUB_OUTPUT: {:?}", &github_output_path);
     let w = writer::Writer::new(github_output_path);
@@ -22,7 +24,7 @@ fn main() {
         version_increment_strategy
     );
 
-    let _github_token = match env::var("INPUT_GITHUB-TOKEN") {
+    let github_token = match env::var("INPUT_GITHUB-TOKEN") {
         Ok(value) => value,
         Err(_) => {
             eprintln!("GITHUB_TOKEN is empty!");
@@ -33,5 +35,55 @@ fn main() {
     let version = "0.1.0";
     let output_text = format!("semver={version}");
     eprintln!("Writing: {}", output_text);
-    w.write("semver", &version);
+    w.write("semver", version);
+
+    let github_client = Octocrab::builder().personal_token(github_token).build()?;
+
+    let roma_version =
+        get_release_version(&github_client, "dexwritescode", "release-on-merge-action").await;
+    eprintln!("Roma version to increment {}", &roma_version);
+
+    let octocrab_version = get_release_version(&github_client, "XAMPPRocky", "octocrab").await;
+
+    eprintln!("Octocrab version to bump {}", octocrab_version);
+
+    Ok(())
+}
+
+async fn get_release_version(github_client: &Octocrab, owner: &str, repo: &str) -> String {
+    github_client
+        .repos(owner, repo)
+        .releases()
+        .get_latest()
+        .await
+        .map_or_else(
+            |e| match e {
+                Error::GitHub { ref source, .. } => {
+                    if source.message.eq_ignore_ascii_case("Not Found") && source.errors.is_none() {
+                        get_default_version()
+                    } else {
+                        eprintln!("Could not get the version.");
+                        eprintln!("Error: {:?}", &e);
+                        exit(1);
+                    }
+                }
+                _ => {
+                    eprintln!("Could not get the version.");
+                    eprintln!("Error: {:?}", &e);
+                    exit(1);
+                }
+            },
+            |r| r.tag_name,
+        )
+}
+
+fn get_default_version() -> String {
+    let default_version = "v0.1.0".to_string();
+    match env::var("INPUT_DEFAULT-VERSION") {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("inputs.default-version not set. Using {}", default_version);
+            default_version
+        }
+    }
 }
--- src/writer.rs
+++ src/writer.rs
@@ -1,20 +1,14 @@
 use std::fs::write;
 
 pub struct Writer {
-    path: String
+    path: String,
 }
 
 impl Writer {
     pub fn new(path: String) -> Writer {
-        Writer {
-            path,
-        }
+        Writer { path }
     }
 
-    pub fn write_value(&self, value: &str) {
-        write(&self.path, value).unwrap();
-    }
-    
     pub fn write(&self, key: &str, value: &str) {
         write(&self.path, format!("{key}={value}")).unwrap();
     }
