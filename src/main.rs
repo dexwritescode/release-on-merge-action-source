fbd597d7531f485c36df009ab5e2d4ba25550002
Dex <dexwritescode@proton.me>
2023-10-10 19:28:53 -0400

    Create release module

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use octocrab::{Error, Octocrab};
-use semver::Semver;
-use std::process::exit;
 use std::str::FromStr;
 
 pub mod config;
 use config::Config;
+pub mod release;
+use release::Release;
 pub mod semver;
+use semver::Semver;
 pub mod writer;
 
 #[tokio::main]
@@ -13,48 +13,17 @@ async fn main() -> octocrab::Result<()> {
     let config = Config::new();
     eprintln!("Config: {:?}", &config);
 
-    let w = writer::Writer::new(&config.github_output_path);
-
-    let github_client = Octocrab::builder()
-        .personal_token(config.github_token.0.clone())
-        .build()?;
-
-    let release = get_release_version(&github_client, &config).await;
-    eprintln!("Release version {:?}", &release);
+    let rel = Release::new(&config);
+    let release = rel.get_release_version().await;
+    eprintln!("Retrieved release version {:?}", &release);
     let new_release = match release {
         Some(v) => v.increment(&config.increment_strategy),
         None => Semver::from_str(&config.default_version).unwrap(),
     };
     eprintln!("Incremented version {}", &new_release);
-    eprintln!("Writing: {:?}", new_release.to_string());
+
+    let w = writer::Writer::new(&config.github_output_path);
     w.write("semver", &new_release.to_string());
 
     Ok(())
-}
-
-async fn get_release_version(github_client: &Octocrab, config: &Config) -> Option<Semver> {
-    github_client
-        .repos(&config.owner, &config.repo)
-        .releases()
-        .get_latest()
-        .await
-        .map_or_else(
-            |e| match e {
-                Error::GitHub { ref source, .. } => {
-                    if source.message.eq_ignore_ascii_case("Not Found") && source.errors.is_none() {
-                        None
-                    } else {
-                        eprintln!("Could not get the version.");
-                        eprintln!("Error: {:?}", &e);
-                        exit(1);
-                    }
-                }
-                _ => {
-                    eprintln!("Could not get the version.");
-                    eprintln!("Error: {:?}", &e);
-                    exit(1);
-                }
-            },
-            |r| Semver::from_str(&r.tag_name).ok(),
-        )
 }
---
+++ src/release.rs
@@ -0,0 +1,59 @@
+use crate::Config;
+use crate::Semver;
+use octocrab::{Error, Octocrab};
+use std::{process::exit, str::FromStr};
+
+pub struct Release {
+    client: Octocrab,
+    repo: String,
+    owner: String,
+}
+
+impl Release {
+    pub fn new(config: &Config) -> Release {
+        let client = match Octocrab::builder()
+            .personal_token(config.github_token.0.clone())
+            .build()
+        {
+            Ok(c) => c,
+            Err(e) => {
+                eprintln!("Error building client {:?}", e);
+                exit(1);
+            }
+        };
+        Release {
+            client,
+            repo: config.repo.clone(),
+            owner: config.owner.clone(),
+        }
+    }
+
+    pub async fn get_release_version(&self) -> Option<Semver> {
+        self.client
+            .repos(&self.owner, &self.repo)
+            .releases()
+            .get_latest()
+            .await
+            .map_or_else(
+                |e| match e {
+                    Error::GitHub { ref source, .. } => {
+                        if source.message.eq_ignore_ascii_case("Not Found")
+                            && source.errors.is_none()
+                        {
+                            None
+                        } else {
+                            eprintln!("Could not get the version.");
+                            eprintln!("Error: {:?}", &e);
+                            exit(1);
+                        }
+                    }
+                    _ => {
+                        eprintln!("Could not get the version.");
+                        eprintln!("Error: {:?}", &e);
+                        exit(1);
+                    }
+                },
+                |r| Semver::from_str(&r.tag_name).ok(),
+            )
+    }
+}
