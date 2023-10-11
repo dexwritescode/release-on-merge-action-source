2ee811acc82bc9a6863eac75ffe8a55a0a9c29c5
Dex <dexwritescode@proton.me>
2023-10-11 19:19:58 -0400

    Override tag prefix

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,15 @@ async fn main() -> octocrab::Result<()> {
     eprintln!("Config: {:?}", &config);
 
     let rel = Release::new(&config);
-    let release = rel.get_release_version().await;
-    eprintln!("Retrieved version {:?}", &release);
-    let default = Semver::from_str(&config.default_version).unwrap();
-    let new_release = release.map_or(default, |v| v.increment(&config.increment_strategy));
-    eprintln!("Incremented version {}", &new_release);
+    let latest_tag = rel.get_latest_tag().await;
+    eprintln!("Retrieved version {:?}", &latest_tag);
+    let default_tag = Semver::from_str(&config.get_default_tag()).unwrap();
+    let new_tag = latest_tag.map_or(default_tag, |v| v.increment(&config.increment_strategy));
+    eprintln!("Incremented version {}", &new_tag);
 
     let w = writer::Writer::new(&config.github_output_path);
-    w.write("version", &new_release.to_string());
+    w.write("version", &new_tag.get_version());
+    w.write("tag", &new_tag.get_tag());
 
     Ok(())
 }
