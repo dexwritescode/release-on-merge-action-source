024d845d301552026c7fe78db737516f4618f2b2
Dex <dexwritescode@proton.me>
2023-10-10 20:01:58 -0400

    Use map_or instead of match (#16)

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,13 @@ async fn main() -> octocrab::Result<()> {
 
     let rel = Release::new(&config);
     let release = rel.get_release_version().await;
-    eprintln!("Retrieved release version {:?}", &release);
-    let new_release = match release {
-        Some(v) => v.increment(&config.increment_strategy),
-        None => Semver::from_str(&config.default_version).unwrap(),
-    };
+    eprintln!("Retrieved version {:?}", &release);
+    let default = Semver::from_str(&config.default_version).unwrap();
+    let new_release = release.map_or(default, |v| v.increment(&config.increment_strategy));
     eprintln!("Incremented version {}", &new_release);
 
     let w = writer::Writer::new(&config.github_output_path);
-    w.write("semver", &new_release.to_string());
+    w.write("version", &new_release.to_string());
 
     Ok(())
 }
